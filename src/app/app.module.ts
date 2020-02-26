465b2a0e8ec1c80de0d014dfca649a3b99ec85a2
Javier <jmartin@grupoloyal.com>
2020-02-26 10:28:36 +0100

    Modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyCoreModule } from 'src/my-core';
+import { MainModule } from './main/main.module';
+import { SecurityModule } from './security/security.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MyCoreModule, MainModule, SecurityModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
