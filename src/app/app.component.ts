1c979784fb30b705444521a24a4d0b9a76a3d8ee
Javier <jmartin@grupoloyal.com>
2020-02-26 11:18:33 +0100

    servicios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoggerService } from 'src/my-core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'curso en soria';
+
+  constructor(private out: LoggerService) {
+  }
+  ngOnInit(): void {
+    this.out.error('Esto es un error');
+    this.out.warn('Esto es un warn');
+    this.out.info('Esto es un info');
+    this.out.log('Esto es un log');
+  }
+
+
 }
--- src/my-core/index.ts
+++ src/my-core/index.ts
@@ -1,1 +1,2 @@
 export { MyCoreModule } from './my-core.module';
+export { LoggerService, } from './services/logger.service';
